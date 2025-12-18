009d408811c0a113385a0fd44c6cfa8f7592c70f
modatt <mariam.odat97@gmail.com>
2025-12-18 18:42:39 +0300

    cpp00, ex00 is done

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 20:49:03 by marvin            #+#    #+#             */
+/*   Updated: 2025/10/08 20:49:03 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+// using namespace std;
+
+int     main(int ac, char **av)
+{
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return 1;
+    }
+    for (int i = 1; i < ac; i++)
+    {
+        std::string arg = av[i];
+        std::transform(arg.begin(), arg.end(), arg.begin(), ::toupper);
+        std::cout << arg;
+        if (i + 1 != ac)
+            std::cout << " ";
+    }
+    return 0;
+}
